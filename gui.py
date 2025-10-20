320011162cb2366637c9bcbc2c1df771771cdbe3
sarahgoburdhun <sarahgoburdhun2002@gmail.com>
2025-10-20 20:38:11 +0400

    fix: indent upload and video updqte methods into gui class to resolve AttributeError

--- gui.py
+++ gui.py
@@ -26,27 +26,29 @@ class GUI:
         Button(root, text="Upload Image", command=self.upload_image).pack(pady=5)
         Button(root, text="Start Live Video", command=self.start_video_stream).pack(pady=5)
         Button(root, text="Stop Video", command=self.stop_video_stream).pack(pady=5)
+    
+    def upload_image(self):
+        #open file dialog
+        filename = filedialog.askopenfilename(filetypes=[("Image Files", "*.png *.jpg *.jpeg")])
+        if filename:
+            self.upload_image(filename) #dislay selected image
+    
+    def update_video(self):
+        #updates the frames for real-time camera feed
+        if self.running and self.cap:
+            ret, frame = self.cap.read()
+            if ret:
+                #saves the frame temporarily for classification
+                temp_path = "frame.jpg"
+                cv2.imwrite(temp_path, frame)
+    
+                rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(rgb_frame).resize((1200, 1200))
+                imgtk = ImageTk.PhotoImage(image=img)
+                #updates with new frame
+                self.image_label.imgtk = imgtk
+                self.image_label.configure(image=imgtk)
+    
+            self.root.after(50, self.update_video)  #sets time between frames to be every 50 ms
 
-def upload_image(self):
-    #open file dialog
-    filename = filedialog.askopenfilename(filetypes=[("Image Files", "*.png *.jpg *.jpeg")])
-    if filename:
-        self.display_image(filename) #dislay selected image
 
-def update_video(self):
-    #updates the frames for real-time camera feed
-    if self.running and self.cap:
-        ret, frame = self.cap.read()
-        if ret:
-            #saves the frame temporarily for classification
-            temp_path = "frame.jpg"
-            cv2.imwrite(temp_path, frame)
-
-            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img = Image.fromarray(rgb_frame).resize((1200, 1200))
-            imgtk = ImageTk.PhotoImage(image=img)
-            #updates with new frame
-            self.image_label.imgtk = imgtk
-            self.image_label.configure(image=imgtk)
-
-        self.root.after(50, self.update_video)  #sets time between frames to be every 50 ms
